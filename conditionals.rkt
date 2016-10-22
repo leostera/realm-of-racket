c845d157681782640c3e0a0234fa78b0f4f481f6
Leandro Ostera <leandro@ostera.io>
2016-10-22 12:04:36 +0200

    Auto-save: Sat Oct 22 12:04:36 CEST 2016

--- conditionals.rkt
+++ conditionals.rkt
@@ -2,6 +2,12 @@
 (zero? 42)
 (zero? 0)
 (integer? 8129)
+(real? 10)
+(real? (sqrt -1))
+(rational? 2/3)
+(rational? (sqrt -1))
+(exact-integer? 1.0)
+(exact-integer? 100))
 
 ; symbol checks
 (symbol? 3)
